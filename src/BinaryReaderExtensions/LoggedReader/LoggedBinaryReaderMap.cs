3eaff33790e049f541894fc36db831f55407330c
Lasse D <lasse@dyrsted.dk>
2022-04-22 18:29:48 +0200

    Documentation

--- src/BinaryReaderExtensions/LoggedReader/LoggedBinaryReaderMap.cs
+++ src/BinaryReaderExtensions/LoggedReader/LoggedBinaryReaderMap.cs
@@ -27,21 +27,19 @@ namespace ldy985.BinaryReaderExtensions.LoggedReader
         }
 
         /// <summary>
-        /// SaveToFile
+        /// SaveToFileMask. Saves to a file that has the same length as the map. With each count clamped to one byte.
         /// </summary>
         /// <param name="filepath"></param>
         /// <exception cref="IOException"></exception>
         /// <exception cref="System.ObjectDisposedException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
-        public void SaveToFile(string filepath)
+        public void SaveToFileMask(string filepath)
         {
             using (FileStream fileStream = File.OpenWrite(filepath))
             using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
             {
                 foreach (uint u in Map)
-                {
                     binaryWriter.Write(u <= 255 ? (byte)u : (byte)255);
-                }
             }
         }
     }
